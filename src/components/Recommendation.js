65a5329f84238b42334782a57ff1fca09163f325
Priscilla <comptepro@mbpdepriscilla.home>
2023-05-26 19:13:45 +0200

    first commit

---
+++ src/components/Recommendation.js
@@ -0,0 +1,19 @@
+function Recommendation() {
+    const currentMonth = new Date().getMonth()
+    const isSpring = currentMonth >= 2 && currentMonth <= 5
+    return (
+       
+           isSpring ? (
+        <div>C'est le printemps, rempotez 🌱</div>
+    ) : (
+        <div>Ce n'est pas le moment de rempoter</div>
+    )   
+        )
+     
+    
+    }
+     
+
+export default Recommendation;
+        
+
