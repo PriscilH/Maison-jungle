e1d6569abf44c9e05be0fc977682dd9706aceecf
Priscilla <comptepro@mbpdepriscilla.home>
2023-05-30 17:51:05 +0200

    add component categories

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,30 +1,46 @@
 import { useState } from 'react';
 
 function Cart({ cart, updateCart }) {
-  const monsteraPrice = 8
   const [isOpen, setIsOpen] = useState(true)
-    
-  return isOpen ? (
-    <div className='lmj-cart'>
-			<button
-				className='lmj-cart-toggle-button'
-				onClick={() => setIsOpen(false)}
-			>
-				Fermer
-			</button>
-			<h2>Panier</h2>
-			<h3>Total : {monsteraPrice * cart}€</h3>
-			<button onClick={() => updateCart(0)}>Vider le panier</button>
-		</div>
-	) : (
-		<div className='lmj-cart-closed'>
-			<button
-				className='lmj-cart-toggle-button'
-				onClick={() => setIsOpen(true)}
-			>
-				Ouvrir le Panier
-			</button>
-		</div>
+  const total = cart.reduce(
+	(acc, plantType) => acc + plantType.amount * plantType.price,
+	0
+)
+return isOpen ? (
+	<div className='lmj-cart'>
+		<button
+			className='lmj-cart-toggle-button'
+			onClick={() => setIsOpen(false)}
+		>
+			Fermer
+		</button>
+		{cart.length > 0 ? (
+			<div>
+				<h2>Panier</h2>
+				<ul>
+					{cart.map(({ name, price, amount }, index) => (
+						<div key={`${name}-${index}`}>
+							{name} {price}€ x {amount}
+						</div>
+					))}
+				</ul>
+				<h3>Total :{total}€</h3>
+				<button onClick={() => updateCart([])}>Vider le panier</button>
+			</div>
+		) : (
+			<div>Votre panier est vide</div>
+		)}
+	</div>
+) : (
+	<div className='lmj-cart-closed'>
+		<button
+			className='lmj-cart-toggle-button'
+			onClick={() => setIsOpen(true)}
+		>
+			Ouvrir le Panier
+		</button>
+	</div> 
+  
 )
       
     }
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -4,9 +4,10 @@ function handleClick(plantName) {
 	alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
 }
 
-const PlantItem = ({ id, cover, name, water, light }) => {
+const PlantItem = ({ cover, name, water, light, price }) => {
   return (
-    <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+    <li className='lmj-plant-item' onClick={() => handleClick(name)}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
