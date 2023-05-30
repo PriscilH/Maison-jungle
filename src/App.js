efd318f187a5b10969967c506dc67fea19adc1e8
Priscilla <comptepro@mbpdepriscilla.home>
2023-05-30 16:29:25 +0200

    add state for basket

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
-// import Cart from './components/Cart';
+import Cart from './components/Cart';
 import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      {/* <Cart/> */}
-      <ShoppingList/>
+      <div className='lmj-layout-inner'>
+        <Cart/>
+        <ShoppingList/>
+      </div>
       <Footer/>
     </div>
   );
