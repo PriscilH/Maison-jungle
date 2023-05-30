5575477e0258dd0c604f8b8a978f104a443ade55
Priscilla <comptepro@mbpdepriscilla.home>
2023-05-30 16:48:48 +0200

    add props

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Cart from './components/Cart';
 import ShoppingList from './components/ShoppingList';
+import { useState } from 'react';
 
 function App() {
+  const [cart, updateCart] = useState([])
+
   return (
     <div className="App">
       <Banner/>
       <div className='lmj-layout-inner'>
-        <Cart/>
-        <ShoppingList/>
+        <Cart cart={cart} updateCart={updateCart}/>
+        <ShoppingList cart={cart} updateCart={updateCart}/>
       </div>
       <Footer/>
     </div>
